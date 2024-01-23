0b8e4c90fdee2673ccacd70f7b2e528773d4bfe7
Reus <victor.villalva2003@hotmail.com>
2024-01-22 23:33:30 -0600

    Version 0.1

--- src/pages/Garrick.jsx
+++ src/pages/Garrick.jsx
@@ -1,7 +1,11 @@
 import {useState} from "react";
+import '../assets/styles/garrick.css'
 
 function Garrick (){
     const [string, setString] = useState();
+    const [token, setToken] = useState([]);
+    const [lexama, setLexema] = useState([]);
+
 
     const gramatica = {
         "class" : "classKeyWord",
@@ -42,6 +46,9 @@ function Garrick (){
         let lexema = "";
         let token = "";
 
+        let tokens = []
+        let lexemas = []
+
         for (const match of matches) {
             const element = match[1];
             const isInGramatica = gramatica[element];
@@ -57,10 +64,16 @@ function Garrick (){
                 lexema = element;
             } else {
                 console.log(element, " is not part of the grammar");
+
             }
 
-            console.log(`token= ${token}, lexema = ${lexema}`);
+            console.log(` |Token= ${token}, lexema = ${lexema}`);
+            tokens.push(`| Token = ${token}`);
+            lexemas.push(`| Lexema = ${lexema}`);
+
         }
+        setToken(tokens);
+        setLexema(lexemas);
     }
 
     const  handlerStringChange =(e)=>{
@@ -71,12 +84,38 @@ function Garrick (){
 
     return(
         <>
-            <div>
-                <input onChange={handlerStringChange}/>
-                <br/>
-                <button onClick={analizar}> Analizar </button>
-            </div>
-
+            <section className="uno">
+                <header>
+                    <h1>Analizador Lexico</h1>
+                    <p className='txt_garrick'>Garrick</p>
+                </header>
+            </section>
+            <section className="dos">
+                <div className="ingreso_datos">
+                    <div className='ingreso_caracteres'>
+                        <input type="text" onChange={handlerStringChange}/>
+                        <button onClick={analizar}>Analizar</button>
+                    </div>
+                    <div className="datos">
+                        <div className="tipo">
+                            <p className="tipo_txt"> | Tokens</p>
+                            <div className="tokens">
+                                {token.map((token, index) => (
+                                    <p key={index}>{token}</p>
+                                ))}
+                            </div>
+                        </div>
+                        <div className="tipo">
+                            <p className="tipo_txt"> | Lexemas</p>
+                            <div className="lexemas">
+                                {lexama.map((lexama, index) => (
+                                    <p key={index}>{lexama}</p>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
