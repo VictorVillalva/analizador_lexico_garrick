242fae2d7b25ac4a77a7abc39e93afa1cdfef402
Gsuscrist <jago2811@gmail.com>
2024-02-06 20:37:43 -0600

    [add] sintax parse v1

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './normalize.css'
-import Garrick from "./pages/Garrick.jsx";
+import Garrick2 from "./pages/Garrick2.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Garrick/>
+    <Garrick2/>
   </React.StrictMode>,
 )
