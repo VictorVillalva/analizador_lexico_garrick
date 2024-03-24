2a649c15bf2bd831d237d07d53c50da90b6aa26f
Reus <victor.villalva2003@hotmail.com>
2024-03-23 22:22:09 -0600

    [fix] print console our code utils interprete

--- src/utils/interprete/interpreter.js
+++ src/utils/interprete/interpreter.js
@@ -1,47 +1,3 @@
-//
-// import {peg$parse} from "./translate.js";
-// import Swal from "sweetalert2";
-// import {peg$SyntaxError} from "../systax/grammar.js";
-//
-// export function translate (string){
-//     try{
-//         const generateCod = peg$parse(string.replace(/\s/g, ""))
-//         if (generateCod){
-//             Swal.fire({
-//                 title: "codigo correcto!",
-//                 text: string,
-//                 icon: "success"
-//             });
-//             const blob = new Blob([generateCod], { type: 'text/javascript' });
-//             const url = URL.createObjectURL(blob);
-//             const a = document.createElement('a');
-//             a.href = url;
-//             a.download = 'Garrick-v1.js';
-//             a.click();
-//             URL.revokeObjectURL(url);
-//         }
-//
-//
-//     }catch (error) {
-//         if (error instanceof peg$SyntaxError) {
-//             console.error("Error sintáctico:", error.message);
-//             Swal.fire({
-//                 icon: "error",
-//                 title: "Oops...",
-//                 text: error.message,
-//                 footer: '<a>Revise que la gramatica sea la correcta</a>'
-//             });
-//             return false
-//
-//
-//         } else {
-//             console.error("Error al analizar:", error);
-//             return false
-//         }
-//     }
-// }
-
-
 import {peg$parse} from "./translate.js";
 import Swal from "sweetalert2";
 import {peg$SyntaxError} from "../systax/grammar.js";
@@ -55,12 +11,16 @@ export function translate (string){
                 text: string,
                 icon: "success"
             });
-
+            
+            const contentInQuotes = generateCod.match(/"(.*?)"/); // Expresión regular para encontrar contenido entre comillas
+            if (contentInQuotes) {
+                console.log(contentInQuotes[1]); // Imprimir solo el contenido entre comillas
+            }
             // Abrir una nueva ventana y escribir el código generado en ella
-            const newWindow = window.open();
-            newWindow.document.write('<html><head><title>Garrick-v1.js</title></head><body><script>');
-            newWindow.document.write(generateCod);
-            newWindow.document.write('</script></body></html>');
+            // const newWindow = window.open();
+            // newWindow.document.write('<html><head><title>Garrick-v1.js</title></head><body><script>');
+            // newWindow.document.write(generateCod);
+            // newWindow.document.write('</script></body></html>');
         }
 
     } catch (error) {
